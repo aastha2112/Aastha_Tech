10b2ce6e09803f0175cc52caac4e0bf7c4b6ed31
Aastha Lakhanpal <aasthalakhanpal11@gmail.com>
2025-02-13 17:24:14 +0530

    Modified: contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -26,16 +26,26 @@ const Contact = () => {
             </h2>
             <p>aasthalakhanpal11@gmail.com</p>
           </div>
-          <div className="relative bg-[#183850] p-6 rounded-xl shadow-lg  h-30 flex items-center justify-between text-white text-lg font-semibold before:absolute before:inset-0 before:bg-gradient-to-r before:from-blue-500 before:to-transparent before:opacity-20 before:rounded-xl hover:before:opacity-40 mb-5">
-            <h2 className="text-5xl">
+          <div className="relative bg-[#183850] p-6 rounded-xl shadow-lg  h-30 flex items-center justify-between text-white text-lg font-semibold before:absolute before:inset-0 before:bg-gradient-to-r before:from-blue-500 before:to-transparent before:opacity-20 before:rounded-xl hover:before:opacity-40 mb-5 before:pointer-events-none">
+            <a
+              href="https://www.linkedin.com/in/aasthalakhanpal7/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-5xl"
+            >
               <ImLinkedin />
-            </h2>
+            </a>
             <p>/aasthalakhanpal7</p>
           </div>
-          <div className="relative bg-[#183850] p-6 rounded-xl shadow-lg  h-30 flex items-center justify-between text-white text-lg font-semibold before:absolute before:inset-0 before:bg-gradient-to-r before:from-blue-500 before:to-transparent before:opacity-20 before:rounded-xl hover:before:opacity-40 mb-5">
-            <h2 className="text-5xl">
+          <div className="relative bg-[#183850] p-6 rounded-xl shadow-lg  h-30 flex items-center justify-between text-white text-lg font-semibold before:absolute before:inset-0 before:bg-gradient-to-r before:from-blue-500 before:to-transparent before:opacity-20 before:rounded-xl hover:before:opacity-40 mb-5 before:pointer-events-none">
+            <a
+              href="https://github.com/aastha2112"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-5xl"
+            >
               <FaGithub />
-            </h2>
+            </a>
             <p> aastha2112</p>
           </div>
           <div className="relative bg-[#183850] p-6 rounded-xl shadow-lg  h-30 flex items-center justify-between text-white text-lg font-semibold before:absolute before:inset-0 before:bg-gradient-to-r before:from-blue-500 before:to-transparent before:opacity-20 before:rounded-xl hover:before:opacity-40 v">
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Aastha_Tech",
   plugins: [react(), tailwindcss()],
 });
